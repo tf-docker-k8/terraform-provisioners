5feb9aa3baa5d8e722d9ca6e7c1bfd02b26f72b6
Sivakumar Reddy <info@joindevops.com>
2023-07-12 19:46:11 +0530

    added mongo

--- ec2.tf
+++ ec2.tf
@@ -29,48 +29,49 @@ resource "aws_security_group" "allow_tls" {
   }
 }
 
-# resource "aws_instance" "your-wish" {
+resource "aws_instance" "your-wish" {
+    ami = "ami-012b9156f755804f5"
+    # from instance_type map instance will be selected based on the current workspace
+    instance_type = "t3.medium"
+    key_name = aws_key_pair.provisioner.key_name
+    security_groups = [aws_security_group.allow_tls.name]
+    user_data = "${file("scripts/docker.sh")}"
+    # where you are running terraform command
+    provisioner "local-exec" {
+        command = "echo The server's IP address is ${self.public_ip} > public_ip.txt"
+    }
+}
+
+# resource "aws_instance" "remote" {
 #     ami = "ami-012b9156f755804f5"
 #     # from instance_type map instance will be selected based on the current workspace
 #     instance_type = "t3.micro"
 #     key_name = aws_key_pair.provisioner.key_name
+#     security_groups = [aws_security_group.allow_tls.name]
 
-#     # where you are running terraform command
-#     provisioner "local-exec" {
-#         command = "echo The server's IP address is ${self.public_ip} > public_ip.txt"
+#     connection {
+#         type     = "ssh"
+#         user     = "ec2-user"
+#         private_key = file("C:\\Users\\user\\provisioner.pem")
+#         host     = self.public_ip
 #     }
+
+#     # provisioner "remote-exec" {
+#     #     inline = [
+#     #         "touch /tmp/remote.txt",
+#     #         "echo 'this file is created by remote provisioner' > /tmp/remote.txt"
+#     #     ]
+#     # }
+
+#     provisioner "remote-exec" {
+#         script = "scripts/docker.sh"
+#     }
+
+#     # provisioner "remote-exec" {
+#     #     inline = [
+#     #         "id",
+#     #         "sudo amazon-linux-extras install nginx1 -y",
+#     #         "sudo systemctl start nginx"
+#     #     ]
+#     # }
 # }
-
-resource "aws_instance" "remote" {
-    ami = "ami-012b9156f755804f5"
-    # from instance_type map instance will be selected based on the current workspace
-    instance_type = "t3.micro"
-    key_name = aws_key_pair.provisioner.key_name
-    security_groups = [aws_security_group.allow_tls.name]
-
-    connection {
-        type     = "ssh"
-        user     = "ec2-user"
-        private_key = file("C:\\Users\\user\\provisioner.pem")
-        host     = self.public_ip
-    }
-
-    # provisioner "remote-exec" {
-    #     inline = [
-    #         "touch /tmp/remote.txt",
-    #         "echo 'this file is created by remote provisioner' > /tmp/remote.txt"
-    #     ]
-    # }
-
-    provisioner "remote-exec" {
-        script = "scripts/docker.sh"
-    }
-
-    # provisioner "remote-exec" {
-    #     inline = [
-    #         "id",
-    #         "sudo amazon-linux-extras install nginx1 -y",
-    #         "sudo systemctl start nginx"
-    #     ]
-    # }
-}
